092b634990bffd283934850936f4bfc6aa22593d
Valery Sery <Valery_Sery@epam.com>
2016-07-01 12:02:52 +0300

    lesson #4

---
+++ hometask/src/test/java/gray/java/hometask/CalculatorTest.java
@@ -0,0 +1,23 @@
+package gray.java.hometask;
+
+import junit.framework.TestCase;
+
+public class CalculatorTest extends TestCase {
+
+	public void testSetOperation() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetResult() {
+		fail("Not yet implemented");
+	}
+
+	public void testClearResult() {
+		fail("Not yet implemented");
+	}
+
+	public void testOperation() {
+		fail("Not yet implemented");
+	}
+
+}
